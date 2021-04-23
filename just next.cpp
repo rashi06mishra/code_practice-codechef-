be7797676c9b8f77e6d190600fa1184ccc9fe27c
rashi06mishra <54906383+rashi06mishra@users.noreply.github.com>
2021-04-24 01:36:38 +0530

    Update just next.cpp
    DevG likes too much fun to do with numbers. Once his friend Arya came and gave him a challenge, he gave DevG an array of digits which is forming a number currently (will be called as given number). DevG was challanged to find the just next greater number which can be formed using digits of given number. Now DevG needs your help to find that just next greater number and win the challenge.
    Input:
    2
    5
    1 5 4 8 3
    10
    1 4 7 4 5 8 4 1 2 6
    
    Output:
    15834
    1474584162
    
    
    The idea behind the solution is to check if the array is already not in decreasing order
    print "-1" if it is
    otherwise starting from the right side(i.e.) end of the array look for the breakpoint 
    i.e. element from where the array starts decreasing again
    save this element in some variable(say temp) for now it will be needed later
    now siince we have all the elements in are stack push it into an array  and sort it to find position of element just greater than temp
    swap that element with the first element (element at 0th position)
    sort the remaining array and push it back into the main array
    print the main array
    
    
    
    // time complexity - o(n)
    //space complexity - o(n)       -not sure

--- just next.cpp
+++ just next.cpp
@@ -1,39 +1,74 @@
 ```
-#include<bits/stdc++.h>
- 
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
 using namespace std;
- 
-#define ll long long int
- 
-void toNumber(ll array[], int N)
-{
-    
-	for(int i = 0; i < N; i++)
-		cout<<array[i];	
-	cout<<endl;
-}
- 
 
- 
-int main()
-{
-	int t = 0;
-	cin>>t;
- 
-	while(t--)
-	{
-		int N = 0;
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+		int N;
 		cin>>N;
- 
-		ll array[N];
- 
-		for(int i = 0; i < N; i++)
-			cin>>array[i];
- 
-		if(next_permutation(array,array+N))
-		    toNumber(array,N);
-		else
-			cout<<-1<<endl;
+		int temp;
+		vector <int> v;
+		for(int i=0;i<N;i++){
+			cin>>temp;
+			v.push_back(temp);
+		}
+		int k=N-2;
+		for(k;v[k]>=v[k+1];k--){}
+		if(k==(-1)){
+		    cout<<k<<endl;
+		    continue;
+		}
+		int i=N-2;
+		int last = v[N-1];
+		stack <int> st;
+		st.push(last);
+		v.pop_back();
+		for(i;i>=0;i--){
+			if(v[i]<st.top()){
+			    temp = v[i];
+			    st.push(v[i]);
+			    v.pop_back();
+			    break;
+			}
+			else{
+			    st.push(v[i]);
+			    v.pop_back();
+			}
+			
+		}
+		vector <int> v4u;
+		while(!st.empty()){
+			v4u.push_back(st.top());
+			st.pop();
+		}
+		sort(v4u.begin(),v4u.end());
+		int pos;
+		int s = v4u.size();
+		
+		for(int i=0;i<s;i++){
+		    if(temp<v4u[i]){
+		        pos=i;
+		        break;
+		    }
+		    
+		}
+		swap(v4u[0],v4u[pos]);
+		sort(v4u.begin()+1, v4u.end());
+		
+		for(int i=0;i<s;i++){
+		    v.push_back(v4u[i]);
+		}
+		for(int j=0;j<N;j++){
+			cout<<v[j];
+		}
+		cout<<endl;
 	}
+	return 0;
 }
 ```
